4ae35b6cb4a946f6ea8e09ddfafb3aa64f26fa1b
Chunwei Chen <tuxoko@gmail.com>
2015-01-13 22:12:20 +0800

    Handle abd_t in sa_impl.h, sa.c, space_map.c, spa.c, spa_history.c and zil.c
    Signed-off-by: Chunwei Chen <tuxoko@gmail.com>

--- module/zfs/spa.c
+++ module/zfs/spa.c
@@ -1583,7 +1583,7 @@ load_nvlist(spa_t *spa, uint64_t obj, nvlist_t **value)
 	if (error)
 		return (error);
 
-	nvsize = *(uint64_t *)db->db_data;
+	nvsize = *(uint64_t *)ABD_TO_BUF(db->db_data);
 	dmu_buf_rele(db, FTAG);
 
 	packed = kmem_alloc(nvsize, KM_PUSHPAGE | KM_NODEBUG);
@@ -1866,7 +1866,7 @@ spa_load_verify_done(zio_t *zio)
 		else
 			atomic_add_64(&sle->sle_data_count, 1);
 	}
-	zio_data_buf_free(zio->io_data, zio->io_size);
+	zio_data_buf_free(ABD_TO_BUF(zio->io_data), zio->io_size);
 
 	mutex_enter(&spa->spa_scrub_lock);
 	spa->spa_scrub_inflight--;
@@ -1913,7 +1913,7 @@ spa_load_verify_cb(spa_t *spa, zilog_t *zilog, const blkptr_t *bp,
 	spa->spa_scrub_inflight++;
 	mutex_exit(&spa->spa_scrub_lock);
 
-	zio_nowait(zio_read(rio, spa, bp, data, size,
+	zio_nowait(zio_read(rio, spa, bp, BUF_TO_ABD(data), size,
 	    spa_load_verify_done, rio->io_private, ZIO_PRIORITY_SCRUB,
 	    ZIO_FLAG_SPECULATIVE | ZIO_FLAG_CANFAIL |
 	    ZIO_FLAG_SCRUB | ZIO_FLAG_RAW, zb));
@@ -5922,7 +5922,7 @@ spa_sync_nvlist(spa_t *spa, uint64_t obj, nvlist_t *nv, dmu_tx_t *tx)
 
 	VERIFY(0 == dmu_bonus_hold(spa->spa_meta_objset, obj, FTAG, &db));
 	dmu_buf_will_dirty(db, tx);
-	*(uint64_t *)db->db_data = nvsize;
+	*(uint64_t *)ABD_TO_BUF(db->db_data) = nvsize;
 	dmu_buf_rele(db, FTAG);
 }
 
